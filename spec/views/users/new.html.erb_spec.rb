5a60ef88b5cdc09f61e772277244b49185279792
Bjoern Schulte <bjoern.schulte@gmx.de>
2013-09-10 11:37:22 +1200

    Make a basic User model (including secure passwords)

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe "users/new.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
 end
